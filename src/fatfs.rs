1588a62ba44b0089c5494f50d7d5674d658c9c66
Nicolas Far <nicofff@gmail.com>
2017-12-04 08:55:13 -0300

    Initial commit

---
+++ src/fatfs.rs
@@ -0,0 +1,84 @@
+
+use std::fs::File;
+use byteorder::{ByteOrder,LittleEndian};
+use std::str;
+//use std::io;
+use std::io::Read;
+use std::io::{Seek,SeekFrom};
+
+#[derive(Debug)]
+pub struct FatFileSystem {
+	file: File,
+	layout: FileSystemLayout
+}
+
+#[derive(Debug)]
+struct FileSystemLayout {
+	fat_start: u16,
+	clusters_start: u32,
+	sectors_per_cluster: u8,
+	cluster_size: u16,
+	root_dir: u32
+}
+
+#[derive(Debug)]
+struct BootRecord {
+	bytes_per_sector: u16,
+	sectors_per_cluster: u8,
+	reserved_sectors:u16,
+	fat_copies:u8,
+	sectors_per_fat:u32,
+	root_directory_cluster_start:u32,
+	fs_info_sector:u16
+}
+impl FatFileSystem {
+	pub fn new(path: &str) -> FatFileSystem{
+	    let mut file = File::open(path).expect("file not found");
+
+		let boot_record = FatFileSystem::get_boot_record(&mut file);
+
+		let layout = FileSystemLayout {
+			fat_start : boot_record.reserved_sectors,
+			clusters_start: boot_record.reserved_sectors as u32 + (boot_record.fat_copies as u32 * boot_record.sectors_per_fat),
+			sectors_per_cluster: boot_record.sectors_per_cluster,
+			root_dir: boot_record.root_directory_cluster_start,
+			cluster_size: boot_record.sectors_per_cluster as u16 * boot_record.bytes_per_sector
+		};
+		let fs = FatFileSystem {
+			file: file,
+			layout: layout
+		};
+		fs
+	}
+
+	fn get_boot_record(file: &mut File) -> BootRecord{
+
+		let mut boot_record = [0;256];
+		let read = file.read(&mut boot_record).expect("Couldn't read");
+
+		let boot_record = BootRecord {
+			bytes_per_sector : LittleEndian::read_u16(&boot_record[0xB..0xD]),
+			sectors_per_cluster: boot_record[0xD],
+			reserved_sectors : LittleEndian::read_u16(&boot_record[0xE..0x10]),
+			fat_copies: boot_record[0x10],
+			sectors_per_fat: LittleEndian::read_u32(&boot_record[0x24..0x28]),
+			root_directory_cluster_start: LittleEndian::read_u32(&boot_record[0x2c..0x30]),
+			fs_info_sector: LittleEndian::read_u16(&boot_record[0x30..0x32]),
+		};
+		boot_record
+	}
+
+	pub fn read_cluster(&mut self,cluster_number: u32) -> Vec<u8> {
+		let mut ret = vec![0u8;self.layout.cluster_size as usize];
+		println!("{:?}",ret.len() );
+
+		let seek_location = self.layout.clusters_start + cluster_number * self.layout.cluster_size as u32;
+		self.file.seek(SeekFrom::Start(seek_location as u64));
+		let read = self.file.read(&mut ret);
+		println!("{:?}",seek_location as usize );
+		ret
+	}
+
+
+
+}
